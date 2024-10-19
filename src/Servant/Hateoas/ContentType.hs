350c5a5205cf105a07a425319ce0210b27f5c399
bruderj15 <julian.bruder@outlook.com>
2024-10-19 13:47:18 +0200

    main: added ContentTypes HalForms, Uber and Siren

--- src/Servant/Hateoas/ContentType.hs
+++ src/Servant/Hateoas/ContentType.hs
@@ -7,9 +7,21 @@ import qualified Network.HTTP.Media as M
 
 data HALJSON
 data CollectionJSON
+data HALFormsJSON
+data UberJSON
+data SirenJSON
 
 instance Accept HALJSON where
   contentType _ = "application" M.// "hal+json"
 
 instance Accept CollectionJSON where
   contentType _ = "application" M.// "vnd.collection+json"
+
+instance Accept HALFormsJSON where
+  contentType _ = "application" M.// "prs.hal-forms+json"
+
+instance Accept UberJSON where
+  contentType _ = "application" M.// "vnd.amundsen-uber+json"
+
+instance Accept SirenJSON where
+  contentType _ = "application" M.// "vnd.siren+json"
