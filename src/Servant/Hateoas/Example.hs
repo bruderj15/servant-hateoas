987824dc894349f7fd0b6468d36cce83af7be763
bruderj15 <julian.bruder@outlook.com>
2024-10-19 16:47:22 +0200

    main: bundled export

--- src/Servant/Hateoas/Example.hs
+++ src/Servant/Hateoas/Example.hs
@@ -1,1 +1,25 @@
 module Servant.Hateoas.Example where
+
+import Servant.Hateoas.Resource
+import Servant.Hateoas.ContentType
+import Servant
+import Data.Aeson
+import GHC.Generics
+
+-------------- Example for dev --------------
+data User = User
+  { userId :: Int
+  , addressId :: Int
+  } deriving (Generic, Show, Eq, Ord)
+
+instance ToJSON User
+
+instance ToResource api User where
+  toResource _ u@(User _ addrId) = Resource u userLinks
+    where
+      userLinks = pure ("address", "http://host:port/addresses/" <> show addrId)
+
+type UserApi = "user" :> Capture "id" Int :> Get '[HALJSON] (Resource User)
+
+userServer :: Server UserApi
+userServer = \i -> return $ toResource (Proxy @UserApi) $ User i 42
