afb7f95db08b0d0eda2b21f74dadb27e90af615c
bruderj15 <julian.bruder@outlook.com>
2024-10-23 13:15:25 +0200

    main: moved MimeRender instance into ContentType

--- src/Servant/Hateoas/ContentType.hs
+++ src/Servant/Hateoas/ContentType.hs
@@ -9,8 +9,13 @@ module Servant.Hateoas.ContentType
 )
 where
 
+import Servant.Hateoas.Resource
 import Servant.API.ContentTypes
 import qualified Network.HTTP.Media as M
+import Servant.Links
+import Data.Aeson
+import Data.Aeson.KeyMap (singleton)
+import GHC.Exts
 
 data HALJSON
 data CollectionJSON
@@ -20,6 +25,13 @@ data SirenJSON
 
 instance Accept HALJSON where
   contentType _ = "application" M.// "hal+json"
+
+instance ToJSON a => MimeRender HALJSON (Resource a) where
+  mimeRender _ (Resource res lks) = encode $ case toJSON res of
+    Object kvm -> Object $ (singleton "_links" lks') <> kvm
+    v -> v
+    where
+      lks' = object [fromString rel .= object ["href" .= linkURI href] | (rel, href) <- lks]
 
 instance Accept CollectionJSON where
   contentType _ = "application" M.// "vnd.collection+json"
--- src/Servant/Hateoas/Resource.hs
+++ src/Servant/Hateoas/Resource.hs
@@ -1,19 +1,12 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE OverloadedLists #-}
-
 module Servant.Hateoas.Resource
 ( Resource(..)
 , HasResource(..)
 , addRel, addRels
 ) where
 
-import Servant.Hateoas.ContentType
-import Servant.API.ContentTypes
 import Servant.Links
-import Data.Aeson
 import Data.Proxy
 import GHC.Generics
-import GHC.Exts
 
 -- | RESTy Resource representation of a datatype.
 data Resource a = Resource
@@ -21,22 +14,15 @@ data Resource a = Resource
   , links    :: [(String, Link)]
   } deriving (Show, Generic)
 
--- Add a relation to another resource.
+-- | Add a relation to another resource.
 addRel :: (String, Link) -> Resource a -> Resource a
 addRel l (Resource x ls) = Resource x (l:ls)
 {-# INLINE addRel #-}
 
--- Add multiple relations to other resources.
+-- | Add multiple relations to other resources.
 addRels :: [(String, Link)] -> Resource a -> Resource a
 addRels ls' (Resource x ls) = Resource x $ ls' ++ ls
 {-# INLINE addRels #-}
-
-instance ToJSON a => MimeRender HALJSON (Resource a) where
-  mimeRender _ (Resource res lks) = encode $ case toJSON res of
-    Object kvm -> Object $ ["_links" .= lks'] <> kvm
-    v -> v
-    where
-      lks' = object [fromString rel .= object ["href" .= linkURI href] | (rel, href) <- lks]
 
 -- | Datatypes that can be represented as 'Resource's.
 class HasResource api a where
