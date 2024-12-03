947da8659dd83ba25296f5e572dcd79e3b9136cf
bruderj15 <julian.bruder@outlook.com>
2024-12-03 14:07:32 +0100

    rewrite: any equal prefix - not just Symbol

--- src/Servant/Hateoas/Rewrite.hs
+++ src/Servant/Hateoas/Rewrite.hs
@@ -2,11 +2,11 @@ module Servant.Hateoas.Rewrite where
 
 import Servant
 import Data.Kind
-import GHC.TypeLits
 
-type MkHatoeas :: Type -> (Type -> Type) -> Type -> Type
-type family MkHatoeas api res ct :: Type where
-  MkHatoeas (Verb m s _ a) res ct           = Verb m s '[ct] (res a)
-  MkHatoeas (a :<|> b) res ct               = MkHatoeas a res ct :<|> MkHatoeas b res ct
-  MkHatoeas ((path :: Symbol) :> b) res ct  = _
-  MkHatoeas (a :> b) res ct                 = a :> MkHatoeas b res ct
+type MkHateoas :: Type -> (Type -> Type) -> Type -> Type
+type family MkHateoas api res ct :: Type where
+  MkHateoas (Verb m s _ a) res ct                     = Verb m s '[ct] (res a)
+  MkHateoas ((prefix :> a) :<|> (prefix :> b)) res ct = prefix :> (MkHateoas a res ct :<|> MkHateoas b res ct)
+  MkHateoas (a :<|> b) res ct                         = MkHateoas a res ct :<|> MkHateoas b res ct
+  MkHateoas ((prefix :> a) :>   (prefix :> b)) res ct = prefix :> (MkHateoas a res ct :>   MkHateoas b res ct)
+  MkHateoas (a :> b) res ct                           = a :> MkHateoas b res ct
