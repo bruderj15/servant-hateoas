e50d99e6fed40ea7ab21555441e2527ec5ddf5de
bruderj15 <julian.bruder@outlook.com>
2024-12-03 17:24:22 +0100

    rewrite: renamed HateoasifyResponseType to Resourcify

--- src/Servant/Hateoas/Rewrite.hs
+++ src/Servant/Hateoas/Rewrite.hs
@@ -17,9 +17,9 @@ type family Normalize api where
 -- May consider special cases where @a@ in @(Verb _ _ _ a)@ is already a resource.
 -- Force overwrite?
 -- Or simply with the content types resource again?
-type HateoasifyResponseType :: Type -> Type -> Type
-type family HateoasifyResponseType api ct where
-  HateoasifyResponseType (a :<|> b) ct           = HateoasifyResponseType a ct :<|> HateoasifyResponseType b ct
-  HateoasifyResponseType (a :> b) ct             = a :> HateoasifyResponseType b ct
-  HateoasifyResponseType (Verb m s _ a) ct       = Verb m s '[ct] (MkResource ct a)
-  HateoasifyResponseType a _                     = a
+type Resourcify :: Type -> Type -> Type
+type family Resourcify api ct where
+  Resourcify (a :<|> b) ct           = Resourcify a ct :<|> Resourcify b ct
+  Resourcify (a :> b) ct             = a :> Resourcify b ct
+  Resourcify (Verb m s _ a) ct       = Verb m s '[ct] (MkResource ct a)
+  Resourcify a _                     = a
