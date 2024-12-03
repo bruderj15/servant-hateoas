e96abbb0f2c70df2da046116dae01c0e48f300c7
bruderj15 <julian.bruder@outlook.com>
2024-12-03 17:16:15 +0100

    rewrite: just wrap response type in resource for now

--- src/Servant/Hateoas/Rewrite.hs
+++ src/Servant/Hateoas/Rewrite.hs
@@ -5,7 +5,6 @@ module Servant.Hateoas.Rewrite where
 import Servant
 import Servant.Hateoas.Resource
 import Data.Kind
-import Data.Type.Equality (type (==))
 
 type family Normalize api where
   Normalize ((prefix :> a) :<|> (prefix :> b)) = Normalize (prefix :> (Normalize a :<|> Normalize b))
@@ -14,11 +13,13 @@ type family Normalize api where
   Normalize (a :> b)                           = a :> Normalize b
   Normalize a                                  = a
 
+-- For now just wrap response type with the content-types resource.
+-- May consider special cases where @a@ in @(Verb _ _ _ a)@ is already a resource.
+-- Force overwrite?
+-- Or simply with the content types resource again?
 type HateoasifyResponseType :: Type -> Type -> Type
 type family HateoasifyResponseType api ct where
   HateoasifyResponseType (a :<|> b) ct           = HateoasifyResponseType a ct :<|> HateoasifyResponseType b ct
   HateoasifyResponseType (a :> b) ct             = a :> HateoasifyResponseType b ct
-  -- TODO: How to handle if response-type already is some resource type res' with res /= res'?
-  HateoasifyResponseType (Verb m s _ (res a)) ct = Verb m s '[ct] (If (res == MkResource ct) (res a) (MkResource ct a))
   HateoasifyResponseType (Verb m s _ a) ct       = Verb m s '[ct] (MkResource ct a)
   HateoasifyResponseType a _                     = a
