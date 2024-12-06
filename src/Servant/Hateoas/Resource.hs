df1bc6971a905a30ee23627c8532e8a48c110998
bruderj15 <julian.bruder@outlook.com>
2024-12-06 15:53:42 +0100

    rewrite-clean: added `addSelfLink`

--- src/Servant/Hateoas/Resource.hs
+++ src/Servant/Hateoas/Resource.hs
@@ -18,6 +18,9 @@ class Resource res where
   -- | Add a relation @(rel, link)@ to a resource.
   addLink :: (String, Link) -> res a -> res a
 
+addSelfLink :: Resource res => Link -> res a -> res a
+addSelfLink l = addLink ("self", l)
+
 -- | Class for 'Resource's that can embed other resources.
 class Resource res => EmbeddingResource res where
   -- | Embed a resource @b@ with its relation @rel@ as tuple @(rel, b)@.
