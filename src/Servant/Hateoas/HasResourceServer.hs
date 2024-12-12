0b177cc0dab2015b9f97d762f73f08b892820308
bruderj15 <julian.bruder@outlook.com>
2024-12-12 22:10:30 +0100

    rewrite: simplified signature by moving constraints to class

--- src/Servant/Hateoas/HasResourceServer.hs
+++ src/Servant/Hateoas/HasResourceServer.hs
@@ -11,78 +11,39 @@ import Control.Monad.IO.Class
 class HasResourceServer api server m ct where
   getResourceServer ::
     ( MonadIO m
+    , HasHandler api
     , ServerT api m ~ server
     , ServerT (Resourcify api ct) m ~ ResourcifyServer server ct m
     ) => Proxy m -> Proxy ct -> Proxy api -> Proxy server -> ServerT (Resourcify api ct) m
 
 instance {-# OVERLAPPABLE #-}
-  ( HasResourceServer a aServer m ct
-  , HasResourceServer b bServer m ct
+  ( HasResourceServer a aServer m ct, HasHandler a
+  , HasResourceServer b bServer m ct, HasHandler b
   ) => HasResourceServer (a :<|> b) (aServer :<|> bServer) m ct where
   getResourceServer m ct _ _ =
          getResourceServer m ct (Proxy @a) (Proxy @aServer)
     :<|> getResourceServer m ct (Proxy @b) (Proxy @bServer)
 
-instance
-  ( MonadIO m
-  , HasHandler api
-  , ToResource (MkResource ct) p
-  , ResourcifyServer (m p) ct m ~ m ((MkResource ct) p)
-  ) => HasResourceServer api (m p) m ct where
+instance (ToResource (MkResource ct) p, ResourcifyServer (m p) ct m ~ m (MkResource ct p)) => HasResourceServer api (m p) m ct where
   getResourceServer m _ api _ = toResource (Proxy @(MkResource ct)) <$> getHandler m api
 
-instance
-  ( MonadIO m
-  , HasHandler api
-  , ToResource (MkResource ct) p
-  , ResourcifyServer (m p) ct m ~ m ((MkResource ct) p)
-  ) => HasResourceServer api (q -> m p) m ct where
+instance (ToResource (MkResource ct) p, ResourcifyServer (m p) ct m ~ m ((MkResource ct) p)) => HasResourceServer api (q -> m p) m ct where
   getResourceServer m _ api _ q = toResource (Proxy @(MkResource ct)) <$> getHandler m api q
 
-instance
-  ( MonadIO m
-  , HasHandler api
-  , ToResource (MkResource ct) p
-  , ResourcifyServer (m p) ct m ~ m ((MkResource ct) p)
-  ) => HasResourceServer api (q -> r -> m p) m ct where
+instance (ToResource (MkResource ct) p, ResourcifyServer (m p) ct m ~ m (MkResource ct p)) => HasResourceServer api (q -> r -> m p) m ct where
   getResourceServer m _ api _ q r = toResource (Proxy @(MkResource ct)) <$> getHandler m api q r
 
-instance
-  ( MonadIO m
-  , HasHandler api
-  , ToResource (MkResource ct) p
-  , ResourcifyServer (m p) ct m ~ m ((MkResource ct) p)
-  ) => HasResourceServer api (q -> r -> s -> m p) m ct where
+instance (ToResource (MkResource ct) p, ResourcifyServer (m p) ct m ~ m (MkResource ct p)) => HasResourceServer api (q -> r -> s -> m p) m ct where
   getResourceServer m _ api _ q r s = toResource (Proxy @(MkResource ct)) <$> getHandler m api q r s
 
-instance
-  ( MonadIO m
-  , HasHandler api
-  , ToResource (MkResource ct) p
-  , ResourcifyServer (m p) ct m ~ m ((MkResource ct) p)
-  ) => HasResourceServer api (q -> r -> s -> t -> m p) m ct where
+instance (ToResource (MkResource ct) p, ResourcifyServer (m p) ct m ~ m (MkResource ct p)) => HasResourceServer api (q -> r -> s -> t -> m p) m ct where
   getResourceServer m _ api _ q r s t = toResource (Proxy @(MkResource ct)) <$> getHandler m api q r s t
 
-instance
-  ( MonadIO m
-  , HasHandler api
-  , ToResource (MkResource ct) p
-  , ResourcifyServer (m p) ct m ~ m ((MkResource ct) p)
-  ) => HasResourceServer api (q -> r -> s -> t -> u -> m p) m ct where
+instance (ToResource (MkResource ct) p, ResourcifyServer (m p) ct m ~ m (MkResource ct p)) => HasResourceServer api (q -> r -> s -> t -> u -> m p) m ct where
   getResourceServer m _ api _ q r s t u = toResource (Proxy @(MkResource ct)) <$> getHandler m api q r s t u
 
-instance
-  ( MonadIO m
-  , HasHandler api
-  , ToResource (MkResource ct) p
-  , ResourcifyServer (m p) ct m ~ m ((MkResource ct) p)
-  ) => HasResourceServer api (q -> r -> s -> t -> u -> v -> m p) m ct where
+instance (ToResource (MkResource ct) p, ResourcifyServer (m p) ct m ~ m (MkResource ct p)) => HasResourceServer api (q -> r -> s -> t -> u -> v -> m p) m ct where
   getResourceServer m _ api _ q r s t u v = toResource (Proxy @(MkResource ct)) <$> getHandler m api q r s t u v
 
-instance
-  ( MonadIO m
-  , HasHandler api
-  , ToResource (MkResource ct) p
-  , ResourcifyServer (m p) ct m ~ m ((MkResource ct) p)
-  ) => HasResourceServer api (q -> r -> s -> t -> u -> v -> w -> m p) m ct where
+instance (ToResource (MkResource ct) p, ResourcifyServer (m p) ct m ~ m (MkResource ct p)) => HasResourceServer api (q -> r -> s -> t -> u -> v -> w -> m p) m ct where
   getResourceServer m _ api _ q r s t u v w = toResource (Proxy @(MkResource ct)) <$> getHandler m api q r s t u v w
