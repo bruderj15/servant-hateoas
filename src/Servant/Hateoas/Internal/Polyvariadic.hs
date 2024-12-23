eabc7a26b55fa6509c5a75c5cd2c8078164f8f32
bruderj15 <julian.bruder@outlook.com>
2024-12-23 17:02:30 +0100

    rewrite: documented Polyvariadic

--- src/Servant/Hateoas/Internal/Polyvariadic.hs
+++ src/Servant/Hateoas/Internal/Polyvariadic.hs
@@ -4,13 +4,18 @@ module Servant.Hateoas.Internal.Polyvariadic where
 
 import Data.Kind
 
+-- | Type-level function to determine if a type is a function.
 type family IsFun f where
   IsFun (_ -> _) = 'True
   IsFun _        = 'False
 
+-- | Class for polyvariadic composition.
+--
+-- This is copied from the package @erisco/control-dotdotdot@.
 class (b ~ IsFun f) => PolyvariadicComp f b where
   type Return  f   b :: Type
   type Replace f r b :: Type
+  -- | @pcomp f g@ has @g@ consume all arguments and then has @f@ consume the result of @g@.
   pcomp :: (Return f b -> r) -> f -> Replace f r b
 
 instance (False ~ IsFun a) => PolyvariadicComp a 'False where
@@ -23,13 +28,19 @@ instance (PolyvariadicComp b (IsFun b)) => PolyvariadicComp (a -> b) 'True where
   type Replace (a -> b) r 'True = a -> Replace b r (IsFun b)
   pcomp final f = \x -> final `pcomp` f x
 
+-- | Infix for 'pcomp'.
 (...) :: (PolyvariadicComp f b, IsFun f ~ b) => (Return f b -> r) -> f -> Replace f r b
 (...) = pcomp
 infixr 9 ...
 
+-- | Like 'PolyvariadicComp' but allows to consume all arguments twice,
+-- by two functions with the exact same arguments but potentially different return types.
 class (b ~ IsFun f, b ~ IsFun g) => PolyvariadicComp2 f g b where
   type Return2  f g   b :: Type
   type Replace2 f g r b :: Type
+  -- | @pcomp2 f g h@ has each @g@ and @h@ consume all arguments and then has @f@ consume the result of @g@ and @h@.
+  --
+  -- This is highly similar to '(&&&)' from 'Control.Arrow' but for polyvariadic composition.
   pcomp2 :: (Return2 f g b -> r) -> f -> g -> Replace2 f g r b
 
 instance (False ~ IsFun a, IsFun b ~ False) => PolyvariadicComp2 a b 'False where
