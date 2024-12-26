ea1a83f553676fce465fd602b45416c7314740a5
bruderj15 <julian.bruder@outlook.com>
2024-12-26 22:48:07 +0100

    qparam: queries make no prefix path

--- src/Servant/Hateoas/Layer.hs
+++ src/Servant/Hateoas/Layer.hs
@@ -65,9 +65,9 @@ type family GoLayers api stand where
   GoLayers (Sym a                  :> b) prefix = '[ 'Layer prefix '[Sym a]                  GetIntermediate ] ++ GoLayers b (prefix ++ '[Sym a])
   GoLayers (Capture'    mods sym a :> b) prefix = '[ 'Layer prefix '[Capture'    mods sym a] GetIntermediate ] ++ GoLayers b (prefix ++ '[Capture'    mods sym a])
   GoLayers (CaptureAll       sym a :> b) prefix = '[ 'Layer prefix '[CaptureAll       sym a] GetIntermediate ] ++ GoLayers b (prefix ++ '[CaptureAll       sym a])
-  GoLayers (QueryParam' mods sym a :> b) prefix = '[ 'Layer prefix '[QueryParam' mods sym a] GetIntermediate ] ++ GoLayers b (prefix ++ '[QueryParam' mods sym a])
-  GoLayers (QueryParams      sym a :> b) prefix = '[ 'Layer prefix '[QueryParams      sym a] GetIntermediate ] ++ GoLayers b (prefix ++ '[QueryParams      sym a])
-  GoLayers (DeepQuery        sym a :> b) prefix = '[ 'Layer prefix '[DeepQuery        sym a] GetIntermediate ] ++ GoLayers b (prefix ++ '[DeepQuery        sym a])
-  GoLayers (QueryFlag        sym   :> b) prefix = '[ 'Layer prefix '[QueryFlag        sym  ] GetIntermediate ] ++ GoLayers b (prefix ++ '[QueryFlag        sym  ])
+  GoLayers (QueryParam' mods sym a :> b) prefix = '[ 'Layer prefix '[QueryParam' mods sym a] GetIntermediate ] ++ GoLayers b prefix
+  GoLayers (QueryParams      sym a :> b) prefix = '[ 'Layer prefix '[QueryParams      sym a] GetIntermediate ] ++ GoLayers b prefix
+  GoLayers (DeepQuery        sym a :> b) prefix = '[ 'Layer prefix '[DeepQuery        sym a] GetIntermediate ] ++ GoLayers b prefix
+  GoLayers (QueryFlag        sym   :> b) prefix = '[ 'Layer prefix '[QueryFlag        sym  ] GetIntermediate ] ++ GoLayers b prefix
   GoLayers (a                      :> b) prefix = GoLayers b (prefix ++ '[a])
   GoLayers _ _ = '[]
