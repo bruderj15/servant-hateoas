6847c18a196613f99450375c2fb692d596d0bc96
Andreas Ländle <969523+alaendle@users.noreply.github.com>
2026-06-24 09:10:04 +0200

    Document status quo.

--- test/Servant/Hateoas/ExampleSpec.hs
+++ test/Servant/Hateoas/ExampleSpec.hs
@@ -2,7 +2,12 @@
 
 module Servant.Hateoas.ExampleSpec (spec) where
 
-import           Servant.Hateoas.Example (apiApp)
+import           Data.Type.Equality      ((:~:) (Refl))
+import           Servant                 (Capture, Capture', Get, JSON,
+                                          QueryParam, (:<|>), (:>))
+import           Servant.Hateoas         (Title)
+import           Servant.Hateoas.Example (Address, Api, User, apiApp)
+import           Servant.Hateoas.Layer   (Normalize)
 import           Test.Hspec
 import           Test.Hspec.Wai
 import           Test.Hspec.Wai.Matcher  (bodyEquals)
@@ -13,3 +18,10 @@ spec = with (pure apiApp) $
     describe "UserGetOne (GET /api/user/:id)" $
       it "returns the requested user as a HAL resource" $
         get "/api/user/1" `shouldRespondWith` 200 { matchBody = bodyEquals "{\"_embedded\":{},\"_links\":{\"address\":{\"href\":\"/api/address/0\",\"type\":\"application/hal+json\"},\"self\":{\"href\":\"/api/user/1\",\"title\":\"The user with the given id\",\"type\":\"application/hal+json\"}},\"addressId\":0,\"income\":0,\"usrId\":1}" }
+
+checkNormalize :: Normalize Api :~: ((
+      "api" :> "user" :> Title "The user with the given id" :> Capture "id" Int :> Get '[JSON] User
+ :<|> "api" :> "user" :> (Get '[JSON] [User]
+                          :<|> "query" :> QueryParam "addrId" Int  :> QueryParam "income"  Double  :> Get '[JSON] User))
+ :<|> "api" :> "address" :> Capture' '[] "id" Int :> Get '[JSON] Address)
+checkNormalize = Refl
